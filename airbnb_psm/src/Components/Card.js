d0402256d04121808923430501454e2f793e89f5
PrabhaShankar-23 <87959932+PrabhaShankar-23@users.noreply.github.com>
2022-02-25 19:12:48 +0530

    card element feature added

--- airbnb_psm/src/Components/Card.js
+++ airbnb_psm/src/Components/Card.js
@@ -1,5 +1,12 @@
 import React from "react";
+import zafers from "../images/image 12.png";
 
+// Mental mapping of eachpart before executing such like when making decision for each part.
+// Broken links are seen or we see that import of image file is required rather than links in img element.
 export default function Card() {
-  return <div className="card--section"></div>;
+  return (
+    <div className="card--section">
+      <img src={zafers} alt="Zafers" />
+    </div>
+  );
 }
